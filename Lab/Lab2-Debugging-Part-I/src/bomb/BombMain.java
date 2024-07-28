b9b15e46e087bcf243eec213e0f0bb5b50d76a93
kekeandzeyu <kekeandzeyu@gmail.com>
2024-07-28 18:28:30 +0800

    Finish Homework 0B!

---
+++ Lab/Lab2-Debugging-Part-I/src/bomb/BombMain.java
@@ -0,0 +1,23 @@
+package bomb;
+
+import common.IntList;
+
+public class BombMain {
+    public static void answers(String[] args) {
+        int phase = 2;
+        if (args.length > 0) {
+            phase = Integer.parseInt(args[0]);
+        }
+
+        Bomb b = new Bomb();
+        if (phase >= 0) {
+            b.phase0("39291226");
+        }
+        if (phase >= 1) {
+            b.phase1(IntList.of(0, 9, 3, 0, 8)); // Figure this out too
+        }
+        if (phase >= 2) {
+            b.phase2("-81201430");
+        }
+    }
+}
